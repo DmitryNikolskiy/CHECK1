b404a7dfd41dffdec7d4b23b41e324bb24c2dd3e
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-20 00:44:06 +0300

    test-program-ready

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = { "Hello!", "Seminar", "Sharp", "4", "15", "Good", "Hi!", ":-)" };
+int i = 0;
+int first = 0;
+Console.Write("[");
+while (i < array.Length)
+{
+    if(array[i].Length < 4)
+    {
+        Console.Write($"\"{array[i]}\"");
+        first = i;
+        break;
+    }
+    i++;
+}
+int j = first+1;
+while (j < array.Length)
+{
+    if(array[j].Length < 4)
+    {
+        Console.Write($", \"{array[j]}\"");
+    }
+    j++;
+}
+Console.Write("]");
